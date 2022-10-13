26833170e26f8bc2cacb02c3d45ffe5f833fbbd0
Sanjay400 <107196725+Sanjay400@users.noreply.github.com>
2022-10-13 14:30:16 +0530

    Create Sum of unique elements.java

---
+++ Leetcode 1st 50 problems/Sum of unique elements.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int sumOfUnique(int[] nums) {
+        int[] arr = new int[101];
+        for (int num : nums) {
+            arr[num]++;
+        }
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
