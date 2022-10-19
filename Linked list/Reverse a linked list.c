bb201030ea28759256743c32991813dddca034ee
Sanjay400 <107196725+Sanjay400@users.noreply.github.com>
2022-10-19 12:01:42 +0530

    Create Reverse a linked list.c

---
+++ Linked list/Reverse a linked list.c
@@ -0,0 +1,19 @@
+struct ListNode* reverseList(struct ListNode* head)
+{
+    if(head==NULL || head->next==NULL)
+    {
+        return head;
+    }
+    struct ListNode* headnew=(struct ListNode*)malloc(sizeof(struct ListNode));
+    headnew->val=head->val;
+    headnew->next=NULL;
+    head=head->next;
+    while(head!=NULL)
+    {
+        struct ListNode* tmp=headnew;
+        headnew=head;            
+        head=head->next;
+        headnew->next=tmp;
+    }
+    return headnew;
+}
